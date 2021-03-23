873f8665f96c907e85c233aa8f20fbed805d0e8a
Isaac23-K <ikaptoge@yahoo.com>
2021-03-23 14:50:31 +0300

    added a toast

--- app/src/main/java/com/moringaschool/eathealthy/Recipe.java
+++ app/src/main/java/com/moringaschool/eathealthy/Recipe.java
@@ -2,9 +2,12 @@ package com.moringaschool.eathealthy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,6 +28,14 @@ public class Recipe extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, recipes);
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String recipes = ((TextView)view).getText().toString();
+                Toast.makeText(Recipe.this, recipes, Toast.LENGTH_LONG).show();
+            }
+        });
 
         Intent intent = getIntent();
         String recipe = intent.getStringExtra("recipes");
