e4cc7497c7c3874ad52a7e86a3f9a96956db5881
Isaac23-K <ikaptoge@yahoo.com>
2021-03-23 14:33:15 +0300

    modified recipe .java

--- app/src/main/java/com/moringaschool/eathealthy/Recipe.java
+++ app/src/main/java/com/moringaschool/eathealthy/Recipe.java
@@ -1,8 +1,14 @@
 package com.moringaschool.eathealthy;
 
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+
 public class Recipe extends AppCompatActivity {
+    @BindView(R.id.listView) ListView mListView ;
+
     private String [] recipes = new String[] {"Cooking rice" , "Cooking chicken ", "Making eggs" , "Cooking beef"};
 
 }
