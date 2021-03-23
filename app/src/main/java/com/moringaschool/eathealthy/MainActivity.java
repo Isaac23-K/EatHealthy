598910e742133b41badf4fc0a1bcc1c9730b500d
Isaac23-K <ikaptoge@yahoo.com>
2021-03-23 15:05:40 +0300

    added butterknife.bind in main

--- app/src/main/java/com/moringaschool/eathealthy/MainActivity.java
+++ app/src/main/java/com/moringaschool/eathealthy/MainActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity{
 
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+    @BindView(R.id.findRecipeButton) Button mFindsRecipesButton ;
+    @BindView(R.id.RecipeEditText) EditText mRecipeEditText ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
