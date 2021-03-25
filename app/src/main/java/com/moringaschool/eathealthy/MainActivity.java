05d4ae2ae1b1888d643e0d93366639d65b227898
Isaac23-K <ikaptoge@yahoo.com>
2021-03-25 12:39:01 +0300

    chaged app icon

--- app/src/main/java/com/moringaschool/eathealthy/MainActivity.java
+++ app/src/main/java/com/moringaschool/eathealthy/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -48,5 +50,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(MainActivity.this, recipes, Toast.LENGTH_SHORT).show();
 
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_root, menu);
+        return true;
     }
 }
